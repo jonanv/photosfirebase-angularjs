42ada692a27cd1634bd7add6e7f5f0982cda074f
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-03-27 12:43:28 -0500

    Se agregan rutas para los componentes en el app.routing.module.ts y se elimina plantilla por defecto, se deja el router-outled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Imports components
+import { PhotosComponent } from './components/photos/photos.component';
+import { LoadPhotosComponent } from './components/load-photos/load-photos.component';
+
+const routes: Routes = [
+  { path: 'photos', component: PhotosComponent },
+  { path: 'load', component: LoadPhotosComponent },
+
+  { path: '**', pathMatch: 'full', redirectTo: 'photos' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Import routing
 import { AppRoutingModule } from './app-routing.module';
+
+// Imports components
 import { AppComponent } from './app.component';
 import { PhotosComponent } from './components/photos/photos.component';
 import { LoadPhotosComponent } from './components/load-photos/load-photos.component';
