7bfe1a3ed820c6627a1b80e5dcac6286bbf005f1
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-03-27 15:26:16 -0500

    Se agregan metodos para validar cuando sea arrastrada la imagen

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,22 +1,53 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
 
+  @Input() public files: FileItem[] = [];
   @Output() private mouseOver: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
 
   @HostListener('dragover', ['$event'])
-  public onDragEnter(event: any) {
+  public onDragEnter(event: any): void {
     this.mouseOver.emit(true);
   }
 
   @HostListener('dragleave', ['$event'])
-  public onDragLeave(event: any) {
+  public onDragLeave(event: any): void {
     this.mouseOver.emit(false);
+  }
+
+  // Validation
+  private fileCanBeLoaded(file: File): boolean {
+    if (!this.fileHasAlreadyBeenDragged(file.name) && this.isImage(file.type)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  private preventAndStop(event: any): void {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private fileHasAlreadyBeenDragged(fileName: string): boolean {
+    for(const file of this.files) {
+      if (file.fileName == fileName) {
+        console.log('El archivo' + fileName + ' ya esta agregado');
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private isImage(typeFile: string): boolean {
+    return (typeFile === '' || typeFile === undefined) ? false : typeFile.startsWith('image');
   }
 
 }
